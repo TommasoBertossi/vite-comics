4be53696921824fa24b1c47f5f22114d1be42696
TommasoBertossi <tommasobertossi@gmail.com>
2023-09-15 17:31:56 +0200

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -30,12 +30,56 @@ export default {
 
 <template>
   <footer>
-    <h3>Footer</h3>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <section class="container">
+      <div class="footer-col">
+        <h3 class="title">DC COMICS</h3>
+        <ul>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h3 class="title">SHOP</h3>
+        <ul>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h3 class="title">DC</h3>
+        <ul>
+          <li>Terms of Use</li>
+          <li>Privacy policy</li>
+          <li>Ad Choices</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certicates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h3 class="title">SITES</h3>
+        <ul>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC KIds</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+    </section>
   </footer>
 </template>
 
